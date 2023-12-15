a9a3ac87e8d74caff12c0c5b7a587e4ea933cb24
MsWakesho <wakeshosonja@gmail.com>
2023-12-15 21:05:19 +0300

    Completed Assignments with comments

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function receivesAFunction(callback){
     callback();
-}
+} // This returns a named function, where "callback" is the named function. 
 
 function  returnsANamedFunction (){
     return function namedFunction(){
 
     };
-}
+} // this returns an named function
 
 function returnsAnAnonymousFunction(){
     return function(){
 
     };
-}
+} // This returns an anonymous function, the returned value is a function that doesn't have a name.
